b5a0715e2ee7eba3ad8eb2fe3f6d41efa0e4d148
benya <rungsri.benya@gmail.com>
2018-11-23 15:19:25 +0700

    tried State and Props in the Component

--- src/HelloApp.js
+++ src/HelloApp.js
@@ -1,21 +1,41 @@
 import React from 'react'
 
 class HelloApp extends React.Component {
+      constructor(props) {
+        super(props)
+
+        this.state = {
+          counter: 0
+        }
+
+        this.handleClick = this.handleClick.bind(this)
+      }
+
+       handleClick() {
+          this.setState({
+              counter: this.state.counter + 1
+          })
+        }
+
+
     render() {
 //        return <h1>MILKK! React</h1>
-        return ( // must have <div></div>
-          <div>
-            <h1>Ahoy! React</h1>
-            <p>{this.props.message}</p>
-          </div>
-        )
-        // or you can use this
-//        return (
-//          <React.Fragment>
+//        return ( // must have <div></div> for Props
+//          <div>
 //            <h1>Ahoy! React</h1>
 //            <p>{this.props.message}</p>
-//          </React.Fragment>
+//          </div>
 //        )
+        //for Props, you can use this
+        return (
+          <React.Fragment>
+            <h1>Ahoy! React</h1>
+            <p>{this.props.message}</p>
+            {/* State */}
+            <button onClick={this.handleClick}>Click me!</button>
+            <p>Total click : {this.state.counter}</p>
+          </React.Fragment>
+        )
     }
 }
 
